6247f350c289942baca49cd39df589e02a2239a4
ooney <62164814+ooneykabooey@users.noreply.github.com>
2026-07-25 16:12:08 -0500

    Initial Commit

---
+++ finalprojectlevidaniel/finalprojectlevidanielApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojectlevidanielApp.swift
+//  finalprojectlevidaniel
+//
+//  Created by Levi Daniel on 7/25/26.
+//
+
+import SwiftUI
+
+@main
+struct finalprojectlevidanielApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
